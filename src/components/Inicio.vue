089004d46f801b8cb65b2762c084685f55d00922
Nicolas Lopez <nicolopz471@gmail.com>
2023-10-03 16:45:18 -0300

    se ordena interfaz
    se ordenan los elementos de la pagina de inicio

--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -1,29 +1,51 @@
 <!-- se implementa el inicio que va antes del login -->
 <template>
-    <h1 class="Titulo">
-        Bienvenido a Libro-Juegos
-    </h1>
-    <div> <!-- en este div van los botones de la interfaz -->
-        <v-btn class="botonLogin" variant="tonal">
-            Login
-        </v-btn>
-        <v-btn class="botonSingUp" variant="tonal">
-            SingUp
-        </v-btn>
+    <div class="encabezado">
+        <h1 class="Titulo">
+            Bienvenido a Libro-Juegos
+        </h1>
+        <div> <!-- en este div van los botones de la interfaz -->
+            <v-btn class="botonLogin" variant="tonal">
+                Login
+            </v-btn>
+            <v-btn class="botonSingUp" variant="tonal">
+                SingUp
+            </v-btn>
+        </div>
     </div>
 
     <!-- se implementa la descripcion de la pagina de inicio -->
-    <v-card clas= "descripcion" title="Espacio para explorar tu creatividad" text="Los libro-juegos son libros donde el lector es el protagonista. Estos libros están escritos por un autor donde a estos deberán ingresar su nombre, título de la aventura, una imagen opcional y acompañado de una breve sinopsis. Para crear la historia debe ir agregando las páginas de la aventura una por una, indicando las opciones que puede tomar el lector. El lector puede elegir una acción que permitirá ser redirigido a la página correspondiente. Al elegir ciertas acciones afectara en los finales de la historia, dando así un final malo, buenos y regulares. Si es un final malo, el lector tendrá la opción de volver al inicio y se descartaran las opciones elegidas anteriormente para no repetir el mismo final. Por otro lado, los lectores podrán visualizar las aventuras en una lista, y podrán elegir una historia basándose en sus características, como sinopsis o imagen." variant="tonal"></v-card>
+    <div class="descripcion">
+        <h2>Espacio para explorar tu creatividad</h2>
+        <p>
+            Los libro-juegos son libros donde el lector es el protagonista. Estos libros están escritos por un autor donde a estos deberán ingresar su nombre, título de la aventura, una imagen opcional y acompañado de una breve sinopsis. Para crear la historia debe ir agregando las páginas de la aventura una por una, indicando las opciones que puede tomar el lector. El lector puede elegir una acción que permitirá ser redirigido a la página correspondiente. Al elegir ciertas acciones afectara en los finales de la historia, dando así un final malo, buenos y regulares. Si es un final malo, el lector tendrá la opción de volver al inicio y se descartaran las opciones elegidas anteriormente para no repetir el mismo final.<br><br>Por otro lado, los lectores podrán visualizar las aventuras en una lista, y podrán elegir una historia basándose en sus características, como sinopsis o imagen. 
+        </p>
+    </div>
 </template>
 
-<style>
-    
+<style >   
     .Titulo{
         font-size: 2rem;
     }
 
+    .encabezado{
+        display: flex;
+        justify-content: space-between;
+        padding-left: 2rem;
+    }
+
     .descripcion{
         font-size: 1.25rem;
+        width:2rem;
+        background-color:gray;
+        width:60%;
+        display:flex;
+        flex-direction:column;
+        justify-content:center;
+        align-items:center;
+        padding:1rem;
+        border-radius:15px;
+        margin-left:2%;
     }
 
 </style>
