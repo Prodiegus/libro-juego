d3d6b502f64ecacc1a401dd6faa29fa884043aaa
Nicolas Lopez <nicolopz471@gmail.com>
2023-10-03 17:02:12 -0300

    Ajuste de bonotes

--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -4,7 +4,7 @@
         <h1 class="Titulo">
             Bienvenido a Libro-Juegos
         </h1>
-        <div> <!-- en este div van los botones de la interfaz -->
+        <div class="botones"> <!-- en este div van los botones de la interfaz -->
             <v-btn class="botonLogin" variant="tonal">
                 Login
             </v-btn>
@@ -18,7 +18,7 @@
     <div class="descripcion">
         <h2>Espacio para explorar tu creatividad</h2>
         <p>
-            Los libro-juegos son libros donde el lector es el protagonista. Estos libros están escritos por un autor donde a estos deberán ingresar su nombre, título de la aventura, una imagen opcional y acompañado de una breve sinopsis. Para crear la historia debe ir agregando las páginas de la aventura una por una, indicando las opciones que puede tomar el lector. El lector puede elegir una acción que permitirá ser redirigido a la página correspondiente. Al elegir ciertas acciones afectara en los finales de la historia, dando así un final malo, buenos y regulares. Si es un final malo, el lector tendrá la opción de volver al inicio y se descartaran las opciones elegidas anteriormente para no repetir el mismo final.<br><br>Por otro lado, los lectores podrán visualizar las aventuras en una lista, y podrán elegir una historia basándose en sus características, como sinopsis o imagen. 
+            Los libro-juegos son libros donde el lector es el protagonista. Estos libros están escritos por un autor donde a estos deberán ingresar su nombre, título de la aventura, una imagen opcional y acompañado de una breve sinopsis. Para crear la historia debe ir agregando las páginas de la aventura una por una, indicando las opciones que puede tomar el lector. El lector puede elegir una acción que permitirá ser redirigido a la página correspondiente. Al elegir ciertas acciones afectara en los finales de la historia, dando así un final malo, buenos y regulares. Si es un final malo, el lector tendrá la opción de volver al inicio y se descartaran las opciones elegidas anteriormente para no repetir el mismo final.<br>Por otro lado, los lectores podrán visualizar las aventuras en una lista, y podrán elegir una historia basándose en sus características, como sinopsis o imagen. 
         </p>
     </div>
 </template>
@@ -35,17 +35,27 @@
     }
 
     .descripcion{
+        color: white;
         font-size: 1.25rem;
         width:2rem;
         background-color:gray;
-        width:60%;
+        width:40%;
         display:flex;
         flex-direction:column;
         justify-content:center;
         align-items:center;
-        padding:1rem;
+        padding:2rem;
         border-radius:15px;
         margin-left:2%;
+        margin-top:3%;
     }
 
+    .botones{
+        margin:1%;
+        
+    }
+
+    .botonSingUp{
+        margin-left:1rem;
+    }
 </style>
