2ce1bda8c3a4882048c5484d1a0a27d156dadd50
Nicolas Lopez <nicolopz471@gmail.com>
2023-10-02 23:14:41 -0300

    Se modifican diseños
    se le agrega diseño a la descripcion

--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -10,17 +10,8 @@
             SingUp
         </v-btn>
     </div>
-    <div>
-        <p>
-            Espacio para explorar tu creatividad
-        </p>
-        <p>
-            Los libro-juegos son libros donde el lector es el protagonista. Estos libros están escritos por un autor donde a estos deberán ingresar su nombre, título de la aventura, una imagen opcional y acompañado de una breve sinopsis. Para crear la historia debe ir agregando las páginas de la aventura una por una, indicando las opciones que puede tomar el lector. El lector puede elegir una acción que permitirá ser redirigido a la página correspondiente. Al elegir ciertas acciones afectara en los finales de la historia, dando así un final malo, buenos y regulares. Si es un final malo, el lector tendrá la opción de volver al inicio y se descartaran las opciones elegidas anteriormente para no repetir el mismo final. 
-        </p>
-        <p>
-            Por otro lado, los lectores podrán visualizar las aventuras en una lista, y podrán elegir una historia basándose en sus características, como sinopsis o imagen. 
-        </p>
-    </div>
+    <v-card clas= "descripcion" title="Espacio para explorar tu creatividad" text="Los libro-juegos son libros donde el lector es el protagonista. Estos libros están escritos por un autor donde a estos deberán ingresar su nombre, título de la aventura, una imagen opcional y acompañado de una breve sinopsis. Para crear la historia debe ir agregando las páginas de la aventura una por una, indicando las opciones que puede tomar el lector. El lector puede elegir una acción que permitirá ser redirigido a la página correspondiente. Al elegir ciertas acciones afectara en los finales de la historia, dando así un final malo, buenos y regulares. Si es un final malo, el lector tendrá la opción de volver al inicio y se descartaran las opciones elegidas anteriormente para no repetir el mismo final. Por otro lado, los lectores podrán visualizar las aventuras en una lista, y podrán elegir una historia basándose en sus características, como sinopsis o imagen." variant="tonal"></v-card>
+
 </template>
 
 <style>
@@ -28,4 +19,9 @@
     .Titulo{
         font-size: 2rem;
     }
+
+    .descripcion{
+        
+    }
+
 </style>
