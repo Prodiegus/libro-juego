e3fa26a54df24c05733444eae3f32061e600bac7
rafa <rafajeremias@live.com>
2023-10-04 02:26:34 -0300

    Rework Home-Inicio
    Falta fusionar estas 2 pantallas para definir un prototipo final.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
   import Inicio from './components/Inicio.vue';
   import Login from './components/Login.vue';
+  import Home from './components/Home.vue';
 </script>
 
 <template>
   <Inicio/>
   <Login/>
+  <!-- <Home/> -->
 </template>
 
 <style>
---
+++ src/components/Home.vue
@@ -0,0 +1,78 @@
+<template>
+ <v-app>
+    <v-navigation-drawer v-model="sidebar" app>
+      <v-list>
+        <v-list-tile
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.path">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+
+    <v-toolbar  
+      dark
+      prominent
+      image="https://zeevector.com/wp-content/uploads/Yellow-Banner-Background-HD.png"
+    app>
+      <span class="hidden-sm-and-up">
+        <v-toolbar-side-icon @click="sidebar = !sidebar">
+        </v-toolbar-side-icon>
+      </span>
+      <v-toolbar-title>
+        <router-link to="/" style="cursor: pointer">
+          {{ appTitle }}
+        </router-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn
+          flat
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.path">
+          <v-icon left dark>{{ item.icon }}</v-icon>
+          {{ item.title }}
+        </v-btn>
+      </v-toolbar-items>
+   
+    </v-toolbar>
+    
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+
+    <v-img
+    height with cover
+    src="http://www.fqdigital.com.ar/images/2021/10/22/imagenes-de-alta-calidad-fqdigital.jpg"
+></v-img>
+  </v-app>
+
+</template>
+
+<script>
+
+
+export default {
+  name: "App",
+  data(){
+    return {
+      appTitle: 'Bienvenidos a LibroJuegos',
+      sidebar: false,
+      menuItems: [
+          { title: 'Sign Up', path: '/signup' },
+          { title: 'Log In', path: '/signin' }
+     ]
+    }
+  },
+};
+</script>
+<style>
+
+
+</style>
