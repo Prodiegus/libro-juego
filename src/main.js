5f903aba8b77486a8abdc3f1098a22a1368840ed
Victor Cornejo <vcornejo19@alumnos.utalca.cl>
2023-10-04 13:05:51 -0300

    Intento de redirecciones
    se estudia y se trata de implementar la redireccion del home con el login

--- src/main.js
+++ src/main.js
@@ -6,10 +6,22 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import Inicio from './components/Inicio.vue';
+import Login from './components/Login.vue';
+import Home from './components/Home.vue';
 
 const vuetify = createVuetify({
   components,
   directives,
 })
+
+const routes = [
+  {
+    path:'/', components: Inicio
+
+  },{
+    path:'/login', components: Login
+  }
+]
 
 createApp(App).use(vuetify).mount('#app')
